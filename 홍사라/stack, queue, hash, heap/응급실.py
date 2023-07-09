a960476662b758ccd504a402e7f06ed06c91a495
sforsara <135079181+sforsara@users.noreply.github.com>
2023-07-09 18:15:32 +0900

    [ADD][ETC] 응급실 / * / *

---
+++ 홍사라/stack, queue, hash, heap/응급실.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+#sys.stdin=open("input.txt", "r") 
+
+ n, m = map(int, input().split())
+ a = [(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
+ a = deque(a)
+ cnt=0
+
+ while True:
+    cur=a.popleft()
+    if any(cur[1]<x[1] for x in a):
+        a.append(cur)
+    else: 
+        cnt+=1 
+        if cur[0]==m: 
+            print(cnt)
+            break
